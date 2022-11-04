e5ea1318a0ef69b589d125891bd718d5fca0a5a6
EMCJava <loyusum246807952@gmail.com>
2022-11-04 22:55:32 +0800

    Changed database table structure(data NOT updated)

--- create_tables.sql
+++ create_tables.sql
@@ -13,16 +13,26 @@ CREATE TABLE Course (
     description VARCHAR(2000) NOT NULL,
     PRIMARY KEY(course_id)
 );
+
 CREATE TABLE Subclass (
     subclass_id VARCHAR(100) NOT NULL,
     course_id VARCHAR(100) NOT NULL,
-    time1 VARCHAR(100) NOT NULL,
-    time2 VARCHAR(100) NOT NULL,
-    address1 VARCHAR(100) NOT NULL,
-    address2 VARCHAR(100) NOT NULL,
     teacher_message VARCHAR(800) NOT NULL,
+    PRIMARY KEY(subclass_id, course_id, stime, etime, repeat_weekly),
+    FOREIGN KEY(course_id) REFERENCES Course(course_id)
+);
+
+CREATE TABLE SubclassInfo (
+    subclass_info_id INT NOT NULL,
+    subclass_id VARCHAR(100) NOT NULL,
+    course_id VARCHAR(100) NOT NULL,
+    week_day VARCHAR(100) NOT NULL,
+    stime VARCHAR(100) NOT NULL,
+    etime VARCHAR(100) NOT NULL,
+    class_loca VARCHAR(100) NOT NULL,
     zoom_link VARCHAR(500) NOT NULL,
-    PRIMARY KEY(subclass_id, course_id),
+    PRIMARY KEY(subclass_id, course_id, subclass_info_id),
+    FOREIGN KEY(subclass_id) REFERENCES Subclass(subclass_id),
     FOREIGN KEY(course_id) REFERENCES Course(course_id)
 );
 
@@ -33,6 +43,8 @@ CREATE TABLE SubclassEvent(
     type ENUM('lecture_note', 'tutorial_note', 'assignment', 'quiz') NOT NULL,
     name VARCHAR(100) NOT NULL,
     hyperlink VARCHAR(500) NOT NULL,
+    upload_time TIMESTAMP NOT NULL,
+    deadline TIMESTAMP,
     PRIMARY KEY(subclass_event_id, course_id, subclass_id), 
     FOREIGN KEY(course_id) REFERENCES Course(course_id),
     FOREIGN KEY(subclass_id) REFERENCES Subclass(subclass_id)
